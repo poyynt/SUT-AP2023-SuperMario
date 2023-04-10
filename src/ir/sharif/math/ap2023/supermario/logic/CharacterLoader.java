e34c710ec74adaebb16b08a6c243fd8ba787deaa
Parsa Torbati <ptorbatii@gmail.com>
2023-04-10 14:46:46 +0430

    CharacterLoader caching

--- src/ir/sharif/math/ap2023/supermario/logic/CharacterLoader.java
+++ src/ir/sharif/math/ap2023/supermario/logic/CharacterLoader.java
@@ -7,31 +7,40 @@ import ir.sharif.math.ap2023.supermario.models.GameCharacter;
 import java.io.*;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.HashMap;
 import java.util.List;
 
 public class CharacterLoader {
     private static Gson gson = new Gson();
+    private static List<String> cachedListOfCharacters = null;
+    private static HashMap<String, GameCharacter> cachedCharacters = new HashMap<>();
     public static List<String> getListOfCharacters() {
+        if (cachedListOfCharacters != null) {
+            return cachedListOfCharacters;
+        }
         try {
             URI resourceURI = CharacterLoader.class.getResource("/characters/list.json").toURI();
             File resourceFile = new File(resourceURI);
             FileReader resourceFileReader = new FileReader(resourceFile);
             TypeToken<List<String>> typeToken = new TypeToken<List<String>>(){};
-            List<String> listOfCharacters = gson.fromJson(resourceFileReader, typeToken);
-            return listOfCharacters;
+            cachedListOfCharacters = gson.fromJson(resourceFileReader, typeToken);
+            return cachedListOfCharacters;
         } catch (URISyntaxException | FileNotFoundException | NullPointerException e) {
             throw new RuntimeException(e);
         }
     }
 
     public static GameCharacter getCharacter(String name) {
+        if (cachedCharacters.get(name) != null) {
+            return cachedCharacters.get(name);
+        }
         try {
             URI resourceURI = CharacterLoader.class.getResource("/characters/" + name + ".json").toURI();
             File resourceFile = new File(resourceURI);
             FileReader resourceFileReader = new FileReader(resourceFile);
             TypeToken<GameCharacter> typeToken = new TypeToken<GameCharacter>(){};
-            GameCharacter gameCharacter = gson.fromJson(resourceFileReader, typeToken);
-            return gameCharacter;
+            cachedCharacters.put(name, gson.fromJson(resourceFileReader, typeToken));
+            return cachedCharacters.get(name);
         } catch (URISyntaxException | FileNotFoundException e) {
             throw new RuntimeException(e);
         }
