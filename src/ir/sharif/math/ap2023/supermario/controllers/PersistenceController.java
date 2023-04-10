5d311a90c66d840cadd77acf889c13f2b8fe588b
Parsa Torbati <ptorbatii@gmail.com>
2023-04-10 19:46:26 +0430

    custom serializer / deserializer for characters to only reflect names

--- src/ir/sharif/math/ap2023/supermario/controllers/PersistenceController.java
+++ src/ir/sharif/math/ap2023/supermario/controllers/PersistenceController.java
@@ -1,21 +1,42 @@
 package ir.sharif.math.ap2023.supermario.controllers;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import com.google.gson.JsonSyntaxException;
+import com.google.gson.*;
 import com.google.gson.reflect.TypeToken;
 import com.google.gson.stream.JsonReader;
 import com.google.gson.stream.JsonWriter;
+import ir.sharif.math.ap2023.supermario.logic.CharacterLoader;
+import ir.sharif.math.ap2023.supermario.models.GameCharacter;
 import ir.sharif.math.ap2023.supermario.models.State;
 import ir.sharif.math.ap2023.supermario.models.User;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 
 public class PersistenceController {
-    private static Gson gson = new Gson();
+    private static Gson gson;
+    static {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+
+        gsonBuilder.registerTypeAdapter(GameCharacter.class, new JsonSerializer<GameCharacter>() {
+            @Override
+            public JsonElement serialize(GameCharacter gameCharacter, Type type, JsonSerializationContext jsonSerializationContext) {
+                return new JsonPrimitive(gameCharacter.getName());
+            }
+        });
+
+        gsonBuilder.registerTypeAdapter(GameCharacter.class, new JsonDeserializer<GameCharacter>() {
+
+            @Override
+            public GameCharacter deserialize(JsonElement jsonElement, Type type, JsonDeserializationContext jsonDeserializationContext) throws JsonParseException {
+                return CharacterLoader.getCharacter(jsonElement.getAsString());
+            }
+        });
+
+        gson = gsonBuilder.create();
+    }
 
     private static String usersFilename = "data/users.json";
 
