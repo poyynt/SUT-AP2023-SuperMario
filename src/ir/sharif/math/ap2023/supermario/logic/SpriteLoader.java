30d33716b522cf5e1ec1bfba6086404dba67b7ff
Parsa Torbati <ptorbatii@gmail.com>
2023-04-11 14:45:30 +0330

    sprite loader agnostic to format

--- src/ir/sharif/math/ap2023/supermario/logic/SpriteLoader.java
+++ src/ir/sharif/math/ap2023/supermario/logic/SpriteLoader.java
@@ -8,7 +8,12 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.awt.image.ImageObserver;
 import java.awt.image.ImageProducer;
+import java.io.File;
+import java.io.FilenameFilter;
 import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -16,25 +21,30 @@ public class SpriteLoader {
 
     private static Map<String, BufferedImage> cache = new HashMap<>();
 
-    public static Image loadSpriteWithName(String spriteName) {
+    public static Image loadSpriteWithName(String category, String spriteName) {
         try {
             if (cache.get(spriteName) == null) {
+                File dir = new File(SpriteLoader.class.getResource("/sprites/character/").toURI());
+                String filenameWithExtension = dir.list(
+                        (dir1, name) -> name.startsWith(spriteName + ".")
+                )[0];
                 cache.put(
                         spriteName,
                         ImageIO.read(
-                                SpriteLoader.class.getResource("/sprites/characters/" + spriteName + ".png")
+                                SpriteLoader.class.getResource(
+                                        "/sprites/" + category + "/" + filenameWithExtension)
                         )
                 );
             }
 
             return cache.get(spriteName);
         }
-        catch (IOException e) {
+        catch (IOException | URISyntaxException e) {
             throw new RuntimeException(e);
         }
     }
 
     public static Image loadSpriteForCharacter(GameCharacter c) {
-        return loadSpriteWithName(c.getName());
+        return loadSpriteWithName("character", c.getName());
     }
 }
