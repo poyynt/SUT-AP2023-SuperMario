ceaa742b11803144ffd4111ea25be0dc7087f192
Parsa Torbati <ptorbatii@gmail.com>
2023-06-10 20:52:26 +0330

    start Title audio on game start

--- src/main/java/supermario/Main.java
+++ src/main/java/supermario/Main.java
@@ -1,11 +1,16 @@
 package supermario;
 
+import supermario.controllers.AudioController;
 import supermario.controllers.GameLoop;
 import supermario.controllers.PersistenceController;
 import supermario.views.StartMenu;
 
+import javax.sound.sampled.Clip;
+
 public class Main {
     public static void main(String[] args) {
+        AudioController.playWavAudioOnChannel("background", "Title", Clip.LOOP_CONTINUOUSLY);
+
         PersistenceController.load();
 
         StartMenu startMenu = StartMenu.getInstance();
