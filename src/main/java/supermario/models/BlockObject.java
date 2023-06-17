eaf0361617a40e27fd5b22aabda2c24e34bee170
Parsa Torbati <ptorbatii@gmail.com>
2023-06-17 15:02:14 +0330

    block object assigns random variable to item if null

--- src/main/java/supermario/models/BlockObject.java
+++ src/main/java/supermario/models/BlockObject.java
@@ -1,27 +1,28 @@
 package supermario.models;
 
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.random.RandomGenerator;
+
 public class BlockObject {
     public final int x, y;
     public final BlockType type;
-    public ItemType item;
+    public final ItemType item;
 
-
-    public BlockObject() {
-        this.x = 0;
-        this.y = 0;
-        this.type = null;
-        this.item = null;
-    }
-    public BlockObject(int x, int y, BlockType type) {
-        this.x = x;
-        this.y = y;
-        this.type = type;
-    }
 
     public BlockObject(int x, int y, BlockType type, ItemType item) {
         this.x = x;
         this.y = y;
         this.type = type;
+        if (item == null) {
+            ItemType[] pool = {
+                    ItemType.COIN, ItemType.COIN, ItemType.COIN, ItemType.COIN,
+                    ItemType.FLOWER, ItemType.FLOWER, ItemType.FLOWER,
+                    ItemType.MUSHROOM, ItemType.MUSHROOM,
+                    ItemType.STAR
+            };
+            item = pool [ThreadLocalRandom.current().nextInt(pool.length)];
+        }
         this.item = item;
     }
 }
