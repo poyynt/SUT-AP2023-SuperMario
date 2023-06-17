59514e928e307f03c2c12bc301efd5aa0f9f1bbe
Parsa Torbati <ptorbatii@gmail.com>
2023-06-17 15:04:10 +0330

    update SectionObject model

--- src/main/java/supermario/models/SectionObject.java
+++ src/main/java/supermario/models/SectionObject.java
@@ -3,7 +3,9 @@ package supermario.models;
 import java.util.List;
 
 public class SectionObject {
-    public List<BlockObject> blocks;
     public int length;
     public int time;
+    public List<BlockObject> blocks;
+    public List<PipeObject> pipes;
+    public PipeObject spawnPipe = null;
 }
