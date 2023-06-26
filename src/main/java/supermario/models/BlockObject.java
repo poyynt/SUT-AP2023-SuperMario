88ce68be7acd262011a6a77f201749ac1ce67c6b
Parsa Torbati <ptorbatii@gmail.com>
2023-06-26 20:28:37 +0330

    add block object hit function

--- src/main/java/supermario/models/BlockObject.java
+++ src/main/java/supermario/models/BlockObject.java
@@ -1,5 +1,7 @@
 package supermario.models;
 
+import supermario.logic.MapHandler;
+
 import java.awt.*;
 import java.util.Random;
 import java.util.concurrent.ThreadLocalRandom;
@@ -10,6 +12,7 @@ public class BlockObject {
     public final BlockType type;
     public final ItemType item;
     private transient final Rectangle hitBox;
+    private transient int state = 0;
 
 
     public BlockObject(int x, int y, BlockType type, ItemType item) {
@@ -31,5 +34,41 @@ public class BlockObject {
 
     public Rectangle getHitBox() {
         return hitBox;
+    }
+
+    public void gotHit() {
+        switch (type) {
+            case SIMPLE -> {
+                MapHandler.sectionObject.blocks.remove(this);
+                State.getCurrentGame().addScore(1);
+            }
+            case COIN -> {
+                ItemObject item = new ItemObject(this.x * 32, (this.y - 1) * 32, ItemType.COIN);
+                MapHandler.sectionObject.items.add(item);
+                BlockObject newBlock = new BlockObject(x, y, BlockType.SIMPLE, null);
+                MapHandler.sectionObject.blocks.remove(this);
+                MapHandler.sectionObject.blocks.add(newBlock);
+                State.getCurrentGame().addScore(1);
+            }
+            case COINS -> {
+                if (state < 5) {
+                    State.getCurrentGame().addCoins(1);
+                    state++;
+                }
+                else {
+                    BlockObject newBlock = new BlockObject(x, y, BlockType.EMPTY, null);
+                    MapHandler.sectionObject.blocks.remove(this);
+                    MapHandler.sectionObject.blocks.add(newBlock);
+                }
+            }
+            case QUESTION -> {
+                ItemObject item = new ItemObject(this.x * 32, (this.y - 1) * 32, this.item);
+                MapHandler.sectionObject.items.add(item);
+                BlockObject newBlock = new BlockObject(x, y, BlockType.EMPTY, null);
+                MapHandler.sectionObject.blocks.remove(this);
+                MapHandler.sectionObject.blocks.add(newBlock);
+                State.getCurrentGame().addScore(1);
+            }
+        }
     }
 }
