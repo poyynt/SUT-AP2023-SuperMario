6cc65c4e431fbd6a4ad3d0947704696ca0aae593
Parsa Torbati <ptorbatii@gmail.com>
2023-06-29 15:34:46 +0330

    enemy getHitBox and getHit

--- src/main/java/supermario/models/EnemyObject.java
+++ src/main/java/supermario/models/EnemyObject.java
@@ -1,5 +1,7 @@
 package supermario.models;
 
+import java.awt.*;
+
 public class EnemyObject {
     public final int x, y;
     public final EnemyType type;
@@ -8,5 +10,13 @@ public class EnemyObject {
         this.x = x;
         this.y = y;
         this.type = type;
+    }
+
+    public Rectangle getHitBox() {
+        return new Rectangle(x, y, 32, 32);
+    }
+
+    public void getHit() {
+        // TODO
     }
 }
