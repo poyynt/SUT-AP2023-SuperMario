da8e2146c5aac31303af9d7b7c37cb6ed13524b9
Parsa Torbati <ptorbatii@gmail.com>
2023-06-29 17:24:59 +0330

    implement GoombaCollisionHandler

---
+++ src/main/java/supermario/logic/GoombaCollisionHandler.java
@@ -0,0 +1,28 @@
+package supermario.logic;
+
+import supermario.models.EnemyObject;
+import supermario.models.State;
+
+import java.awt.*;
+
+public class GoombaCollisionHandler extends EnemyCollisionHandler {
+
+    public GoombaCollisionHandler(EnemyObject target) {
+        super(target);
+    }
+
+    @Override
+    public void update() {
+        if (State.getCurrentGame() == null || !State.getCurrentGame().isRunning())
+            return;
+        Rectangle hitBox = target.getHitBox();
+        Rectangle headHitBox = target.getHeadHitBox();
+        Rectangle marioHitBox = State.getCurrentGame().getPlayer().getHitBox();
+        if (headHitBox.intersects(marioHitBox))
+            target.getKilled();
+        else if (hitBox.intersects(marioHitBox)) {
+            System.out.println("DD");
+            State.getCurrentGame().getPlayer().getHit();
+        }
+    }
+}
