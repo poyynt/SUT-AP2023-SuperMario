6679448b279dd36a5f59fcdafb473d6c751e2c45
Parsa Torbati <ptorbatii@gmail.com>
2023-06-29 16:27:41 +0330

    enemy gravity handler, hit by bullet

---
+++ src/main/java/supermario/logic/EnemyGravityHandler.java
@@ -0,0 +1,60 @@
+package supermario.logic;
+
+import supermario.controllers.Loop;
+import supermario.models.BlockObject;
+import supermario.models.ItemObject;
+import supermario.models.ItemType;
+import supermario.models.State;
+
+import java.awt.*;
+import java.awt.geom.Rectangle2D;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+
+public class EnemyGravityHandler extends Loop {
+    private List<EnemyGravityHandler> instances = new CopyOnWriteArrayList<>();
+    private GravityItem target;
+    private double vy = 0;
+    private double g = 0.08;
+
+    public EnemyGravityHandler(GravityItem target) {
+        super(60.0);
+        for (EnemyGravityHandler instance: instances)
+            if (instance.target == target)
+                instance.stop();
+        instances.removeIf(i -> i.target == target);
+        this.target = target;
+        instances.add(this);
+    }
+
+    @Override
+    public void update() {
+        if (State.getCurrentGame() == null || !State.getCurrentGame().isRunning())
+            return;
+        int y = target.getY();
+        Rectangle hitBox = target.getHitBox();
+        hitBox.translate(0, (int) vy);
+        boolean collides = false;
+        if (MapHandler.sectionObject != null) {
+            for (BlockObject b : MapHandler.sectionObject.blocks) {
+                Rectangle blockHitBox = b.getHitBox();
+                if (blockHitBox.intersects(hitBox)) {
+                    int oc = hitBox.outcode(blockHitBox.getCenterX(), blockHitBox.getCenterY());
+                    if (
+                            (vy >= 0 && oc == Rectangle2D.OUT_BOTTOM) ||
+                                    (vy < 0 && oc == Rectangle2D.OUT_TOP))
+                        collides = true;
+                }
+            }
+        }
+        if (y + vy >= 20 * 32) {
+            collides = true;
+        }
+        if (collides)
+            vy = 0.;
+        else {
+            target.setY((int) (y + vy));
+            vy += g;
+        }
+    }
+}
--- src/main/java/supermario/models/EnemyObject.java
+++ src/main/java/supermario/models/EnemyObject.java
@@ -1,22 +1,58 @@
 package supermario.models;
 
+import supermario.logic.EnemyGravityHandler;
+import supermario.logic.GravityItem;
+import supermario.logic.MapHandler;
+
 import java.awt.*;
 
-public class EnemyObject {
-    public final int x, y;
+public class EnemyObject implements GravityItem {
+    public int x, y;
     public final EnemyType type;
+    private EnemyGravityHandler gravityHandler;
 
     public EnemyObject(int x, int y, EnemyType type) {
         this.x = x;
         this.y = y;
         this.type = type;
+        this.gravityHandler = new EnemyGravityHandler(this);
+        this.gravityHandler.start();
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
     }
 
     public Rectangle getHitBox() {
         return new Rectangle(x, y, 32, 32);
     }
 
+    public Rectangle getHeadHitBox() {
+        return new Rectangle(x + 8, y, 16, 16);
+    }
+
     public void getHit() {
-        // TODO
+        switch (type) {
+            case GOOMBA -> State.getCurrentGame().addScore(1);
+            case KOOPA -> State.getCurrentGame().addScore(2);
+            case SPINY -> State.getCurrentGame().addScore(3);
+        }
+        switch (type) {
+            case GOOMBA, KOOPA, SPINY -> State.getCurrentGame().addCoins(3);
+        }
+        MapHandler.sectionObject.enemies.remove(this);
+        this.gravityHandler.stop();
     }
 }
